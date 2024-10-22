10903dc750a15512ce14755934f779816e0e8ca2
Khalil McFarlane <kmac36960@gmail.com>
2024-10-21 17:17:14 -0700

    Added skeleton code for getting post info

---
+++ shonen-blog/src/app/posts/[postId]/route.tsx
@@ -0,0 +1,24 @@
+/*
+Code for retrieving post from DB would be similar to this below:
+
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const postId = searchParams.get('postId');
+ 
+  try {
+    if (!postId) throw new Error('PostId required');
+    await sql`INSERT INTO Post (postId) VALUES (${postId});`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+ 
+  const posts = await sql`SELECT * FROM Posts;`;
+  return NextResponse.json({ posts }, { status: 200 });
+
+  // Import into [postId]/page.tsx and display info 
+}
+
+*/
