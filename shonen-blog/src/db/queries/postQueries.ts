43d0bd86b69faac13d62f70e73d825503d42845e
Khalil McFarlane <kmac36960@gmail.com>
2024-11-21 20:39:24 -0500

    Create postQueries.ts

---
+++ shonen-blog/src/db/queries/postQueries.ts
@@ -0,0 +1,39 @@
+import type { Post } from "prisma/prisma-client";
+//import prisma from "../lib/prisma";
+import { prisma } from "@/db";
+import { notFound } from "next/navigation";
+
+export async function getPosts(): Promise<Post[]> {
+  const posts = await prisma.post.findMany({
+    orderBy: [
+      {
+        updatedAt: "desc",
+      },
+    ],
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+
+  return posts;
+}
+export async function getPostbyId(postId: string): Promise<Post | null> {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+
+  if (!post) {
+    notFound();
+  }
+
+  return post;
+}
