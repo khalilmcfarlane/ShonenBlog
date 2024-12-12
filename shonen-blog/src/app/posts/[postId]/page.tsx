3f5a325b24edb69e55ac1c1e97843b9969b7a915
Khalil McFarlane <kmac36960@gmail.com>
2024-12-12 14:29:17 -0800

    Used Mantine's text for pages

--- shonen-blog/src/app/posts/[postId]/page.tsx
+++ shonen-blog/src/app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import layoutStyles from "../../css/Layout.module.css";
-
+import classes from "../../css/PostGrid.module.css";
+import { Card, Center, AspectRatio, Image, Text } from "@mantine/core";
+import { notFound } from "next/navigation";
 //import { prisma } from "@/db"
 import { getPostbyId } from "@/db/queries/postSqlQueries";
 
@@ -11,12 +13,25 @@ interface Props {
 
 export default async function Posts({ params }: Props) {
   const post = await getPostbyId(params.postId);
+  if (!post) {
+    notFound();
+  }
+  // change to conditional render if post
   return (
-    <div className={layoutStyles.mainContent}>
-      <h1>Your posts</h1>
-      <h1>{post?.title}</h1>
-      <h2>{post?.author?.username}</h2>
-      <p>{post?.content}</p>
-    </div>
+    <Card>
+      <Text className={classes.title} mt={5} size="xl">
+        {post.title}
+      </Text>
+      <Text c="dimmed" size="l" tt="uppercase" fw={700} mt="md">
+        {post?.author?.username}
+      </Text>
+      <AspectRatio ratio={16 / 9}>
+        <Image src={post.image} alt={post.title} />
+      </AspectRatio>
+
+      <Text className={classes.title} mt={5} size="md">
+        {post.content}
+      </Text>
+    </Card>
   );
 }
