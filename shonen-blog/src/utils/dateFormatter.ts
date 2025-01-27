31774d5e6a9761d567e822ae44ad1e6aee16c43b
Khalil McFarlane <kmac36960@gmail.com>
2025-01-26 19:32:08 -0800

    Fixed date formatting issue

--- shonen-blog/src/utils/dateFormatter.ts
+++ shonen-blog/src/utils/dateFormatter.ts
@@ -1,9 +1,15 @@
 // Format date in US friendly format
+import { format } from "date-fns";
+
 export function formatDate(date: Date | undefined) {
   if (!date) {
     return null;
   }
 
+  const formattedDate = format(date, "MMMM d, yyyy h:mm a");
+  console.log("Formatted date:", formattedDate);
+  return formattedDate;
+  /*
   return new Intl.DateTimeFormat("en-US", {
     month: "long", // Full month name
     day: "numeric", // Day of the month
@@ -12,4 +18,5 @@ export function formatDate(date: Date | undefined) {
     minute: "numeric", // Minute
     hour12: true, // Use 12-hour clock
   }).format(date);
+  */
 }
